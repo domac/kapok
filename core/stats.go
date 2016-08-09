1b705c7c347d63bc82758a123146a53cf2ef01fa
Phil Lee <philcoding@gmail.com>
2016-08-09 14:47:39 +0800

    update qps

--- core/stats.go
+++ core/stats.go
@@ -22,10 +22,12 @@ type Stats struct {
 func (aggStats *Stats) PrintStats(responders int) (res string) {
 	avgThreadDur := aggStats.Duration / time.Duration(responders) //need to average the aggregated duration
 	reqRate := float64(aggStats.NumRequests) / avgThreadDur.Seconds()
+	qpsRate := float64(aggStats.NumRequests) / (float64(responders) * avgThreadDur.Seconds())
 	avgReqTime := aggStats.Duration / time.Duration(aggStats.NumRequests)
 	bytesRate := float64(aggStats.RespSize) / avgThreadDur.Seconds()
 	res += fmt.Sprintf("%v requests in %v, %v read\n", aggStats.NumRequests, avgThreadDur, util.ByteSize{float64(aggStats.RespSize)})
-	res += fmt.Sprintf("Requests/second (QPS):\t%.2f\nTransfer/second:\t%v\nAvg Request Time:\t%v\n", reqRate, util.ByteSize{bytesRate}, avgReqTime)
+	res += fmt.Sprintf("Requests/second:\t%.2f\nTransfer/second:\t%v\nAvg Request Time:\t%v\n", reqRate, util.ByteSize{bytesRate}, avgReqTime)
+	res += fmt.Sprintf("Query Per Second:\t%v\n", qpsRate)
 	res += fmt.Sprintf("Fastest Request:\t%v\n", aggStats.MinRequestTime)
 	res += fmt.Sprintf("Slowest Request:\t%v\n", aggStats.MaxRequestTime)
 	res += fmt.Sprintf("Number of Errors:\t%v\n", aggStats.NumErrs)
